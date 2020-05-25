ef212458728f56df72289ab318080cea48bcdbbb
daydreamwarrior <32808663+daydreamwarrior@users.noreply.github.com>
2020-05-25 14:30:54 +0900

    busts 금액차감 부분 지움

--- BlackJack/BlackJackBoard.py
+++ BlackJack/BlackJackBoard.py
@@ -282,7 +282,6 @@ class BlackJack:
         if self.player.value() > 21:
             self.Lstatus.configure(text="Player Busts")
             PlaySound('Resources/sounds/wrong.wav', SND_FILENAME)
-            self.playerMoney -= self.betMoney
         elif self.dealer.value() > 21:
             self.Lstatus.configure(text="Dealer Busts")
             self.playerMoney += self.betMoney * 2
